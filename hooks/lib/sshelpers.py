95d284304ff5f69d8958b80ef932908f2228a415
jammin <jammin.irving@gmail.com>
2015-03-15 17:07:31 -0700

    Ok, renamed the helper file one more time. Might as well do this early. sshelper.py (changed strong to ss)

--- hooks/lib/sshelpers.py
+++ hooks/lib/sshelpers.py
@@ -3,16 +3,12 @@
 File containing wrapper and helpers functions
 """
 
-# import sys
-# import os
-# import subprocess
-
 
 # returns the essential strongswan packages 
 # other packages are added based on initial launch options
 # other features can added be added later
 # depending on how you want to set up strongswan this can differ
-def packages():
+def ss_packages():
 	ss_essential = [ "libstrongswan" , "strongswan-ike", "strongswan-plugin-openssl"
 	, "strongswan-starter", "strongswan" ]
 
