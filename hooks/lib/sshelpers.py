f0da48a93326d2ca0dabe22eddd50c657cffd2c1
jammin <jammin.irving@gmail.com>
2015-03-22 11:41:02 -0700

    adjusted sshelpers to now contain the sysctl code, the config dictionary is passed in as an argument

--- hooks/lib/sshelpers.py
+++ hooks/lib/sshelpers.py
@@ -8,7 +8,7 @@ File containing wrapper and helpers functions
 # other packages are added based on initial launch options
 # other features can added be added later
 # depending on how you want to set up strongswan this can differ
-def ss_packages():
+def ss_packages( config ):
 	ss_essential = [ "libstrongswan" , "strongswan-ike", "strongswan-plugin-openssl"
 	, "strongswan-starter", "strongswan" ]
 
@@ -16,4 +16,16 @@ def ss_packages():
 	ss_extra = []
 	
 	return (ss_essential + ss_extra)
+
+
+# based on the config passed to this function
+# strongswan will be set up as packet forwarder or not
+# what if the config changes?
+# w
+
+def ss_sysctl( config ):
+
+	if config['ip_forward'] :
+		# modify the sys ctl file
+		
 
