be3844d5a5548579d3285cdd154fe9f2d08b0463
jammin <jammin.irving@gmail.com>
2015-03-23 11:16:06 -0700

    new function for apt-cache

--- hooks/lib/sshelpers.py
+++ hooks/lib/sshelpers.py
@@ -12,29 +12,27 @@ from charmhelper.core import hookenv
 IPV4_FORWARD = "net.ipv4.ip_forward"
 SYSCTL_FILE_PATH = "/etc/sysctl.conf"
 
+
+
+def ss_iptables():
+	pass
+
+
+
+
 # returns the essential strongswan packages 
 # other packages are added based on initial launch options
 # other features can added be added later
 # depending on how you want to set up strongswan this can differ
 def ss_apt_pkgs( config ):
-
-	avail_pkgs = []
-
-	cmd = ["apt-cache", "search", "strongswan"]
-	try:
-		handler = sp.Popen(cmd, stdout=sp.PIPE, stderr=sp.PIPE )
-		data = handler.communicate()[0].decode('utf-8') 
-	except (OSError, sp.TimeoutExpired, ValueError) as error : 
-		hookenv.log(error)
-
-	for s in ( data.split('\n') ):
-		t = s.split(' ')
-		avail_pkgs.append(t[0])
+	avail_pkgs = ss_apt_cache()
 
 	if len(avail_pkgs) > 0 :
 		hookenv.log('Strongswan Packages:')
 		for pkg in avail_pkgs:
 			hookenv.log(pkg)
+	else:
+		hookenv.log('No packages........Something is up....Not throwing exception though')
 
 	# just return 1 package: strongswan.. for now... 
 	return [ "strongswan" ]
@@ -110,5 +108,20 @@ def dict_from_sysctl_file( sysctl_file ):
 
 	return _dict
 
-def ss_iptables():
-	pass
+
+
+def ss_apt_cache():
+	avail_pkgs = []
+
+	cmd = ["apt-cache", "search", "strongswan"]
+	try:
+		handler = sp.Popen(cmd, stdout=sp.PIPE, stderr=sp.PIPE )
+		data = handler.communicate()[0].decode('utf-8') 
+	except (OSError, sp.TimeoutExpired, ValueError) as error : 
+		hookenv.log(error)
+
+	for s in ( data.split('\n') ):
+		t = s.split(' ')
+		avail_pkgs.append(t[0])
+
+	return avail_pkgs
