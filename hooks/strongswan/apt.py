2d91edf447bd97aa537c3c472f29b84a6f0f67be
jammin <jammin.irving@gmail.com>
2015-04-04 20:17:00 -0700

    Added function to update entries in the hosts file. Currently testing.
    When do we actually want to overwrite these changes to the hosts file.
    After the install has completed I would say.

--- hooks/strongswan/apt.py
+++ hooks/strongswan/apt.py
@@ -88,8 +88,31 @@ def get_archive_ip_addrs():
 def update_hosts_file( ip_addr , hostname ):
 	hookenv("INFO:\tAdding {0}\t{1} to /etc/hosts".format(ip_addr, hostname ) )
 
-	with open('/etc/hosts' , 'r+') as hosts :
-		for line in hosts:
-			elem = re.findall('^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\s\w+' , line )
+	aliased_hosts = []
+	output_string = ""
+
+	# open the hosts file
+	# create string to write to the hosts file
+	# add the corresponding entry if it does not exist already.
+	with open('/etc/hosts' , 'r') as hosts_file :
+		for line in hosts_file:
+			elem = re.findall('^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\s\S*' , line )
 			if elem:
-				elem[0].split()
+				if elem[0].split()[1] == hostname:
+					output_string += "{0}\t{1}\n".format(ip_addr, hostname)
+				else:
+					output_string += line
+				aliased_hosts.append(elem[0].split()[1])
+			else:
+				output_string += line
+		if hostname not in aliased_hosts:
+			output_string += "{0}\t{1}\n".format(ip_addr, hostname)
+
+	# write the corresponding changes to the file
+	with open('/etc/hosts', 'w') as hosts_file:
+		hosts_file.write(output_string)
+
+
+
+					
+
