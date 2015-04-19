8ff6f9c58c0f2dba9695ea8cc4ed3b3fc3ecae71
Ben Irving <jammin.irving@gmail.com>
2015-04-18 18:59:27 -0700

    Moved run_apt_command to the util.py file and added the functionality of passing the type of apt-command as an arg for the sake of code readability.

--- hooks/strongswan/archive.py
+++ hooks/strongswan/archive.py
@@ -1,22 +1,17 @@
 
 from charmhelpers.core import hookenv
 from time import sleep
-from strongswan.hosts import update_hosts_file, flush_hosts_file, get_archive_ip_addrs
-from strongswan.constants import PYOPENSSL_DEPENDENCIES
-from strongswan.util import _check_call
-from strongswan.errors import DnsError, NetworkError, AptLockError
+from strongswan.constants import PYOPENSSL_DEPENDENCIES, CONFIG
+from strongswan.util import _check_call, run_apt_command
 from urllib.request import urlretrieve
 
 
 # installs the strongswan packages from the archives.
 def install_strongswan_archives():
 	hookenv.log("Installing Strongswan from the archives" , level=hookenv.INFO )
-	run_apt_command( ["apt-get" , "update", "-qq"] , 30 )
-	cmd = ["apt-get" , "install", "-y", "-qq"]
-	cmd.extend( strongswan_pkgs() )
-	run_apt_command( cmd, 60 )
-
-
+	run_apt_command([], apt_cmd='update' )
+	run_apt_command(['strongswan'], apt_cmd='install', timeout=300 )
+	
 	
 # installs strongswan from the most recent strongswan tarball
 def install_strongswan_version( version ):
@@ -27,55 +22,20 @@ def install_strongswan_version( version ):
 def install_strongswan_github():
 	pass
 
-# runs apt-get command handles dpkg locks and archive server unavailability
-# TODO, i believe apt-get returns if it can't contact a DNS server
-# handle this error appropriately
-def run_apt_command(cmd, timeout_interval ):
-
-	apt_retry_count = 0
-	apt_retry_max = 0
-	apt_retry_wait = 10
-	dpkg_lock_error = 100
-	timed_out = False
-	result = None
-
-	while result is None or result == dpkg_lock_error :
-		try:
-			result = _check_call( cmd, timeout=timeout_interval, quiet=True, fatal=True )
-		
-		# Unable to get that lock
-		except sp.CalledProcessError as e:
-			apt_retry_count += 1
-			if apt_retry_count > apt_retry_max : 
-				raise AptLockError("Unable to acquire DPKG Lock")
-			result = e.returncode
-			sleep(apt_retry_wait)
-
-		# The command has timed out - wonder why - lets try some DNS hacks.
-		except sp.TimeoutExpired:
-			hookenv.log("{0} command has timed out....".format(cmd), level=hookenv.INFO )
-			if not timed_out :
-				timed_out = True
-				dns_entries = get_archive_ip_addrs()
-				if not dns_entries:
-					raise DnsError("Dns Error")
-			if not dns_entries:
-				raise NetworkError("Unable to contact an archive server.")
-			else:
-				_ip = dns_entries.pop()
-				update_hosts_file( _ip , "archive.ubuntu.com" )
-				update_hosts_file( _ip , "security.ubuntu.com" )
-
-
 
 # Installs the PyOpenssl Package into Python 3
 def install_pyOpenSSL():
 	hookenv.log("Installing PyOpenSSL Dependencies" , level=hookenv.INFO )
-	run_apt_command( ["apt-get" , "update", "-qq"] , 30 )
-	cmd = ["apt-get" , "install", "-y", "-qq"]
-	cmd.extend(PYOPENSSL_DEPENDENCIES)
-	run_apt_command(cmd, 300)
-	_check_call( ["pip3", "install" , "pyOpenSSL"] , 
+	
+	#update apt if not done already
+	if CONFIG.get("source") != "archives" :
+		run_apt_command([], apt_cmd='update')
+
+	#install dependencies
+	run_apt_command( PYOPENSSL_DEPENDENCIES, apt_cmd='install', timeout=300 )
+
+	# install into python3 
+	_check_call( [ "pip3", "install" , "pyOpenSSL"] , 
 		fatal=True, 
 		message="Installing pyOpenSSL into Python 3 installation", 
 		quiet=True
--- hooks/strongswan/util.py
+++ hooks/strongswan/util.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python3
 
 import subprocess as sp
 from charmhelpers.core import hookenv
 from strongswan.constants import CHECK, IPTABLES, DELETE
+from strongswan.errors import DnsError, NetworkError, AptLockError
+from strongswan.hosts import update_hosts_file, get_archive_ip_addrs
 
 # wrapper to check_call
 def _check_call( cmd , fatal=False, 
@@ -12,7 +13,7 @@ def _check_call( cmd , fatal=False,
 	hookenv.log("Calling {0}".format(cmd) , level=hookenv.INFO )
 	try:
 		if quiet:
-			return ( sp.check_call( cmd, stdout=sp.DEVNULL, , stderr=sp.DEVNULL, timeout=timeout ) ) 
+			return ( sp.check_call( cmd, stdout=sp.DEVNULL, stderr=sp.DEVNULL, timeout=timeout ) ) 
 		else:
 			return ( sp.check_call( cmd, timeout=timeout ) )
 	except sp.CalledProcessError as err:
@@ -45,3 +46,53 @@ def make_rule(cmd, chain, rule_type):
 		if rule_type == DELETE :
 			cmd[1] = DELETE
 			_check_call(cmd, fatal=True, message="Deleting IPTables rule")
+
+
+
+# runs apt-get command handles dpkg locks and archive server unavailability
+# TODO, i believe apt-get returns if it can't contact a DNS server
+# handle this error appropriately
+def run_apt_command(cmd, timeout=60 , apt_cmd='install'):
+
+	apt_retry_count = 0
+	apt_retry_max = 0
+	apt_retry_wait = 10
+	dpkg_lock_error = 100
+	timed_out = False
+	result = None
+
+	# build the command list
+	cmd.insert(0, '-qq' )
+	if apt_cmd == 'install' :
+		cmd.insert(0, '-y' )
+	cmd.insert(0, apt_cmd)
+	cmd.insert(0, 'apt-get')
+		
+
+	# call the apt command
+	while result is None or result == dpkg_lock_error :
+		try:
+			result = _check_call( cmd, timeout=timeout, quiet=True, fatal=True )
+		
+		# Unable to get that lock
+		except sp.CalledProcessError as e:
+			apt_retry_count += 1
+			if apt_retry_count > apt_retry_max : 
+				raise AptLockError("Unable to acquire DPKG Lock")
+			result = e.returncode
+			sleep(apt_retry_wait)
+
+		# The command has timed out - wonder why - lets try some DNS hacks.
+		except sp.TimeoutExpired:
+			hookenv.log("{0} command has timed out....".format(cmd), level=hookenv.INFO )
+			if not timed_out :
+				timed_out = True
+				dns_entries = get_archive_ip_addrs()
+				if not dns_entries:
+					raise DnsError("Dns Error")
+			if not dns_entries:
+				raise NetworkError("Unable to contact an archive server.")
+			else:
+				_ip = dns_entries.pop()
+				update_hosts_file( _ip , "archive.ubuntu.com" )
+				update_hosts_file( _ip , "security.ubuntu.com" )
