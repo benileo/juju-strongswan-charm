eca3f747dd892758bf3a7fd5fc8ce68d356bac23
jammin <jammin.irving@gmail.com>
2015-04-29 09:41:12 -0700

    added doc_strings to action wrappers

--- hooks/strongswan/actions.py
+++ hooks/strongswan/actions.py
@@ -3,8 +3,11 @@ from strongswan.util import _check_call
 from charmhelpers.core import hookenv
 from json import loads 
 
-# python wrapper for action-get 
 def action_get():
+	"""
+	@params None
+	@return action arguments in json format
+	"""
 	try:
 		data = _check_call([ "action-get", "--format", "json" ], 
 					check_output=True)
@@ -15,8 +18,11 @@ def action_get():
 	return data
 
 
-# python wrapper for action-fail
 def action_fail( message ):
+	"""
+	@params the message to log to juju-log and action-fail command
+	@return None
+	"""
 	cmd = """action-fail "{}" """.format(message) 
 	_check_call( cmd, shell=True )
 	hookenv.log( message, level=hookenv.ERROR )
@@ -25,6 +31,11 @@ def action_fail( message ):
 
 # python wrapper for action-set
 def action_set( **kwargs ):
+	"""
+	@params variable number of key value pairs
+	@return None
+	@description Set key value pairs to return from action
+	"""
 	cmd = "action-set"
 	for key, value in kwargs.items():
 		cmd = """{} {}="{}" """.format(
